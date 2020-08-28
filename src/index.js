80a514a0ba3c408eeaa21a2b0f9a284cce99788b
Gubi <guneedmts@gmail.com>
2020-08-28 22:29:57 +1000

    Change default path to landing-page

--- src/index.js
+++ src/index.js
@@ -26,18 +26,18 @@ import 'assets/scss/now-ui-kit.scss?v=1.4.0';
 import 'assets/demo/demo.css?v=1.4.0';
 import 'assets/demo/nucleo-icons-page-styles.css?v=1.4.0';
 // pages for this kit
-import Index from 'views/Index.js';
+// import Index from 'views/Index.js';
 import NucleoIcons from 'views/NucleoIcons.js';
-import LoginPage from 'views/examples/LoginPage.js';
-import LandingPage from 'views/Index.js';
-import ProfilePage from 'views/examples/ProfilePage.js';
+// import LoginPage from 'views/examples/LoginPage.js';
+import { LandingPage, LoginPage, ProfilePage } from 'views';
+// import ProfilePage from 'views/examples/ProfilePage.js';
 
 const hist = createBrowserHistory();
 
 ReactDOM.render(
   <BrowserRouter history={hist}>
     <Switch>
-      <Route path="/index" render={(props) => <Index {...props} />} />
+      {/* <Route path="/index" render={(props) => <Index {...props} />} /> */}
       <Route
         path="/nucleo-icons"
         render={(props) => <NucleoIcons {...props} />}
@@ -51,8 +51,7 @@ ReactDOM.render(
         render={(props) => <ProfilePage {...props} />}
       />
       <Route path="/login-page" render={(props) => <LoginPage {...props} />} />
-      <Redirect to="/index" />
-      <Redirect from="/" to="/index" />
+      <Redirect to="/landing-page" />
     </Switch>
   </BrowserRouter>,
   document.getElementById('root')
