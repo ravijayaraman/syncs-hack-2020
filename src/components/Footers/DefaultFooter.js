9495f56aeb098d51d5b5288590a331501583636a
Jamie <jram2927@uni.sydney.edu.au>
2020-08-28 22:59:12 +1000

    footer update

--- src/components/Footers/DefaultFooter.js
+++ src/components/Footers/DefaultFooter.js
@@ -13,47 +13,12 @@ function DefaultFooter() {
         <Container>
           <nav>
             <ul>
-              <li>
-                <a
-                  href="https://www.creative-tim.com?ref=nukr-default-footer"
-                  target="_blank"
-                >
-                  Creative Tim
-                </a>
-              </li>
-              <li>
-                <a
-                  href="http://presentation.creative-tim.com?ref=nukr-default-footer"
-                  target="_blank"
-                >
-                  About Us
-                </a>
-              </li>
-              <li>
-                <a
-                  href="http://blog.creative-tim.com?ref=nukr-default-footer"
-                  target="_blank"
-                >
-                  Blog
-                </a>
-              </li>
             </ul>
           </nav>
           <div className="copyright" id="copyright">
-            © {new Date().getFullYear()}, Designed by{" "}
-            <a
-              href="https://www.invisionapp.com?ref=nukr-default-footer"
-              target="_blank"
-            >
-              Invision
-            </a>
-            . Coded by{" "}
-            <a
-              href="https://www.creative-tim.com?ref=nukr-default-footer"
-              target="_blank"
-            >
-              Creative Tim
-            </a>
+            © {new Date().getFullYear()}, Designed by TeamName
+            
+            . Coded by us awesome people
             .
           </div>
         </Container>
