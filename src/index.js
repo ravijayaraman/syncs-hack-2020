330d5ad510eef122da4b4bcc897855d536258753
Gubi <guneedmts@gmail.com>
2020-08-28 22:22:18 +1000

    reformatted

--- src/index.js
+++ src/index.js
@@ -15,47 +15,45 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 
 // styles for this kit
-import "assets/css/bootstrap.min.css";
-import "assets/scss/now-ui-kit.scss?v=1.4.0";
-import "assets/demo/demo.css?v=1.4.0";
-import "assets/demo/nucleo-icons-page-styles.css?v=1.4.0";
+import 'assets/css/bootstrap.min.css';
+import 'assets/scss/now-ui-kit.scss?v=1.4.0';
+import 'assets/demo/demo.css?v=1.4.0';
+import 'assets/demo/nucleo-icons-page-styles.css?v=1.4.0';
 // pages for this kit
-import Index from "views/Index.js";
-import NucleoIcons from "views/NucleoIcons.js";
-import LoginPage from "views/examples/LoginPage.js";
-import LandingPage from "views/examples/LandingPage.js";
-import ProfilePage from "views/examples/ProfilePage.js";
+import Index from 'views/Index.js';
+import NucleoIcons from 'views/NucleoIcons.js';
+import LoginPage from 'views/examples/LoginPage.js';
+import LandingPage from 'views/Index.js';
+import ProfilePage from 'views/examples/ProfilePage.js';
+
+const hist = createBrowserHistory();
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter history={hist}>
     <Switch>
-      <Switch>
-        <Route path="/index" render={(props) => <Index {...props} />} />
-        <Route
-          path="/nucleo-icons"
-          render={(props) => <NucleoIcons {...props} />}
-        />
-        <Route
-          path="/landing-page"
-          render={(props) => <LandingPage {...props} />}
-        />
-        <Route
-          path="/profile-page"
-          render={(props) => <ProfilePage {...props} />}
-        />
-        <Route
-          path="/login-page"
-          render={(props) => <LoginPage {...props} />}
-        />
-        <Redirect to="/index" />
-        <Redirect from="/" to="/index" />
-      </Switch>
+      <Route path="/index" render={(props) => <Index {...props} />} />
+      <Route
+        path="/nucleo-icons"
+        render={(props) => <NucleoIcons {...props} />}
+      />
+      <Route
+        path="/landing-page"
+        render={(props) => <LandingPage {...props} />}
+      />
+      <Route
+        path="/profile-page"
+        render={(props) => <ProfilePage {...props} />}
+      />
+      <Route path="/login-page" render={(props) => <LoginPage {...props} />} />
+      <Redirect to="/index" />
+      <Redirect from="/" to="/index" />
     </Switch>
   </BrowserRouter>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
